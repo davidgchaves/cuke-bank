4f82dff83a9f7ff238d6d36474d1b6270eaef2fa
David G Chaves <davidgchaves@gmail.com>
2014-05-22 10:54:20 +0200

    Make 'When I withdraw $20' (silly) pass

--- features/step_definitions/cash_withdrawal_steps.rb
+++ features/step_definitions/cash_withdrawal_steps.rb
@@ -1,10 +1,8 @@
 # TODO LIST:
 # - We need to implement the Account properly, with unit tests
+# - We need to implement the Teller properly, with unit tests
 
 class Account
-  def initialize
-  end
-
   def deposit(amount)
     @balance = amount
   end
@@ -14,19 +12,25 @@ class Account
   end
 end
 
+class Teller
+  def withdraw_from(account, amount)
+  end
+end
+
 CAPTURE_CASH_AMOUNT = Transform /^\$(\d+)$/ do |digits|
   digits.to_i
 end
 
 Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
-  my_account = Account.new
-  my_account.deposit amount
-  expect(my_account.balance).to eq(amount),
-    "Expected the balance to be #{amount} but it was #{my_account.balance}"
+  @my_account = Account.new
+  @my_account.deposit amount
+  expect(@my_account.balance).to eq(amount),
+    "Expected the balance to be #{amount} but it was #{@my_account.balance}"
 end
 
-When(/^I request \$(\d+)$/) do |amount|
-  pending # express the regexp above with the code you wish you had
+When /^I withdraw (#{CAPTURE_CASH_AMOUNT})/ do |amount|
+  teller = Teller.new
+  teller.withdraw_from @my_account, amount
 end
 
 Then(/^\$(\d+) are dispensed$/) do |amount|
