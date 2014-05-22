7d6f66b20d7ef7024713844e4ba19a2a4cb41346
David G Chaves <davidgchaves@gmail.com>
2014-05-22 10:05:52 +0200

    Make 'Given I have deposited $100 in my account' step (silly) pass

--- features/step_definitions/cash_withdrawal_steps.rb
+++ features/step_definitions/cash_withdrawal_steps.rb
@@ -1,5 +1,21 @@
-Given(/^I have deposited \$(\d+) in my account$/) do |amount|
-  pending # express the regexp above with the code you wish you had
+class Account
+  def initialize
+  end
+
+  def deposit(amount)
+    @balance = amount
+  end
+
+  def balance
+    @balance
+  end
+end
+
+Given /^I have deposited \$(\d+) in my account$/ do |amount|
+  my_account = Account.new
+  my_account.deposit amount.to_i
+  expect(my_account.balance).to eq(amount.to_i),
+    "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
 When(/^I request \$(\d+)$/) do |amount|
