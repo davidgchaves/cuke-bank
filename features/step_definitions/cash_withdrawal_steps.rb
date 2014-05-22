38fb248fc9bc32f692f7360d57ea1011aeb27365
David G Chaves <davidgchaves@gmail.com>
2014-05-22 11:42:52 +0200

    Make 'Successfully withdrawal from an account in credit' scenario pass

--- features/step_definitions/cash_withdrawal_steps.rb
+++ features/step_definitions/cash_withdrawal_steps.rb
@@ -1,6 +1,8 @@
 # TODO LIST:
 # - We need to implement the Account properly, with unit tests
 # - We need to implement the Teller properly, with unit tests
+# - We need to implement the CashSlot properly, with unit tests
+# - Do we really need amount as an argument to the Teller's withdraw_from method?
 
 class Account
   def deposit(amount)
@@ -13,17 +15,40 @@ class Account
 end
 
 class Teller
+  def initialize(cash_slot)
+    @cash_slot = cash_slot
+  end
+
   def withdraw_from(account, amount)
+    @cash_slot.dispense amount
   end
 end
 
-module KnowsMyAccount
+class CashSlot
+  def contents
+    @contents or raise("I'm empty!")
+  end
+
+  def dispense(amount)
+    @contents = amount
+  end
+end
+
+module KnowsMyDomain
   def my_account
     @my_account ||= Account.new
   end
+
+  def cash_slot
+    @cash_slot ||= CashSlot.new
+  end
+
+  def teller
+    @teller ||= Teller.new cash_slot
+  end
 end
 
-World KnowsMyAccount
+World KnowsMyDomain
 
 CAPTURE_CASH_AMOUNT = Transform /^\$(\d+)$/ do |digits|
   digits.to_i
@@ -36,10 +61,9 @@ Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
 end
 
 When /^I withdraw (#{CAPTURE_CASH_AMOUNT})/ do |amount|
-  teller = Teller.new
   teller.withdraw_from my_account, amount
 end
 
-Then(/^\$(\d+) are dispensed$/) do |amount|
-  pending # express the regexp above with the code you wish you had
+Then /(#{CAPTURE_CASH_AMOUNT}) are dispensed$/ do |amount|
+  expect(cash_slot.contents).to eq amount
 end
