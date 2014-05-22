8e0b82446d4391c6ab23d6c4e6216ef90e1f6fd7
David G Chaves <davidgchaves@gmail.com>
2014-05-22 09:29:50 +0200

    Start 'Cash Withdrawal' step definitions

---
+++ features/step_definitions/cash_withdrawal_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I have deposited \$(\d+) in my account$/) do |amount|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I request \$(\d+)$/) do |amount|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^\$(\d+) are dispensed$/) do |amount|
+  pending # express the regexp above with the code you wish you had
+end
