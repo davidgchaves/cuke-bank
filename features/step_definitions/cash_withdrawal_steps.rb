aa052e5d253d5ceac18a09ec220cbd2e0a437bc9
David G Chaves <davidgchaves@gmail.com>
2014-05-22 12:08:02 +0200

    Move KnowMyDomain helper and World call to its own file

--- features/step_definitions/cash_withdrawal_steps.rb
+++ features/step_definitions/cash_withdrawal_steps.rb
@@ -4,22 +4,6 @@
 # - We need to implement the CashSlot properly, with unit tests
 # - Do we really need amount as an argument to the Teller's withdraw_from method?
 
-module KnowsMyDomain
-  def my_account
-    @my_account ||= Account.new
-  end
-
-  def cash_slot
-    @cash_slot ||= CashSlot.new
-  end
-
-  def teller
-    @teller ||= Teller.new cash_slot
-  end
-end
-
-World KnowsMyDomain
-
 Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
   my_account.deposit amount
   expect(my_account.balance).to eq(amount),
