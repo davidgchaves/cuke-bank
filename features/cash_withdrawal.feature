8355d7e73e017243425446aeea72649174356641
David G Chaves <davidgchaves@gmail.com>
2014-05-22 13:56:30 +0200

    Fix Teller's 'withdraw_from' (from the To-Do List)

--- features/cash_withdrawal.feature
+++ features/cash_withdrawal.feature
@@ -3,3 +3,4 @@ Feature: Cash Withdrawal
     Given I have deposited $100 in my account
     When I withdraw $20
     Then $20 are dispensed
+    And the balance of my account is $80
--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -3,3 +3,8 @@ Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
   expect(my_account.balance).to eq(amount),
     "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
+
+Then /^the balance of my account is (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
+  expect(my_account.balance).to eq(amount),
+    "Expected the balance to be #{amount} but it was #{my_account.balance}"
+end
