e7d69210715b9ca02c01b16bb28646b71a12b28a
David G Chaves <davidgchaves@gmail.com>
2014-05-22 14:13:08 +0200

    Apply Kent Beck's "4 criteria for a simple design"

--- features/cash_withdrawal.feature
+++ features/cash_withdrawal.feature
@@ -1,6 +1,6 @@
 Feature: Cash Withdrawal
   Scenario: Successfully withdrawal from an account in credit
-    Given I have deposited $100 in my account
+    Given my account has been credited with $100
     When I withdraw $20
     Then $20 are dispensed
     And the balance of my account is $80
--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -1,7 +1,5 @@
-Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
-  my_account.deposit amount
-  expect(my_account.balance).to eq(amount),
-    "Expected the balance to be #{amount} but it was #{my_account.balance}"
+Given /^my account has been credited with (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
+  my_account.credit amount
 end
 
 Then /^the balance of my account is (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
