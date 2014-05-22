e7186592f31dca56a407aa42e3180ad682ed4b11
David G Chaves <davidgchaves@gmail.com>
2014-05-22 10:14:30 +0200

    Use 'Transform' to remove '.to_i' duplication

--- features/step_definitions/cash_withdrawal_steps.rb
+++ features/step_definitions/cash_withdrawal_steps.rb
@@ -14,10 +14,14 @@ class Account
   end
 end
 
+Transform /^\d+$/ do |number|
+  number.to_i
+end
+
 Given /^I have deposited \$(\d+) in my account$/ do |amount|
   my_account = Account.new
-  my_account.deposit amount.to_i
-  expect(my_account.balance).to eq(amount.to_i),
+  my_account.deposit amount
+  expect(my_account.balance).to eq(amount),
     "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
