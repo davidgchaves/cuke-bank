3f723eeb018bbe87b175581fc34f5f2acc7e12b7
David G Chaves <davidgchaves@gmail.com>
2014-05-22 18:41:08 +0200

    Our tests run now against a web interface

--- lib/cuke_bank.rb
+++ lib/cuke_bank.rb
@@ -35,5 +35,14 @@ end
 
 require 'sinatra'
 get "/" do
-  "Welcome to the Cuke Bank"
+  erb :index
+end
+
+set :cash_slot, CashSlot.new
+set :account do
+  fail "Account has not been set"
+end
+post "/withdraw" do
+  teller = Teller.new settings.cash_slot
+  teller.withdraw_from settings.account, params[:amount].to_i
 end
