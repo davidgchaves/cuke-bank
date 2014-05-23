f2ec80605ed7df9207eca4a0b2b6bc3b4df9c373
David G Chaves <davidgchaves@gmail.com>
2014-05-23 19:09:49 +0200

    Refactor to use a DB through ActiveRecord

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -3,6 +3,6 @@ Given /^my account has been credited with (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
 end
 
 Then /^the balance of my account is (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
-  eventually { expect(my_account.balance).to eq(amount),
+  eventually { expect(my_account.reload.balance).to eq(amount),
                "Expected the balance to be #{amount} but it was #{my_account.balance}" }
 end
